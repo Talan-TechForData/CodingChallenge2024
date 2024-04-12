ba6c954295eeebb19ec770fd2382e3a3c031b653
Andres Ladino <andres.ladino@ratp.fr>
2024-04-12 14:05:13 +0200

    P1: Fix solution

--- equipes/andres_team/P1.py
+++ equipes/andres_team/P1.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+import argparse
 
 p = Path(__file__) / "../../../P1/input.txt"
 p = p.resolve()
 print(p)
 
 with open(p,"r") as f:
-    text = f.readlines()
-
-print(text)
+    for line in f.readlines():
+        print(line)
+        break
