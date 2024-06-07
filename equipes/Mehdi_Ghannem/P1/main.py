96aa57c2b787ca0d40160274bc4faad1f60c99f4
MehdiGhannem <mehdighannem2016@gmail.com>
2024-06-07 11:30:34 +0200

    change

--- equipes/Mehdi_Ghannem/P1/main.py
+++ equipes/Mehdi_Ghannem/P1/main.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 def solve(input_filename, output_filename):
     with open(input_filename, 'r') as f, open(output_filename, 'w') as outfile:
+        count = 0
         for line in f.readlines():
             data = [int(x) for x in line.split()]
             # test case
             ans = 0
-            count = 0
+            
             for i in range(len(data)  - 3):
                 v0 , v1, v2 = data[i + 1], data[i + 2], data[i + 3]
                 if v0 < v1 and v1 > v2:
