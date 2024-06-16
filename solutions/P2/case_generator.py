fc47c2b68c5a052c3127d83cd0e3d081ece21ba7
Andres Ladino <aladinoster@gmail.com>
2024-06-16 23:37:22 +0200

    Solution generators

---
+++ solutions/P2/case_generator.py
@@ -0,0 +1,36 @@
+import random
+import argparse
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Creates a File with Locks")
+
+    parser.add_argument("cases", type=int)
+
+    args = parser.parse_args()
+
+    with open("input_test.txt", "w") as fin, open("output_test.txt", "w") as fout:
+        fin.write(f"{args.cases}\n")
+        for i in range(args.cases):
+            N = random.randint(1, 30)
+            B = random.randint(10, 30)
+            fin.write(f"{N} {B}\n")
+
+            a_i = []
+            for i in range(N):
+                a_i.append(random.randint(1, N))
+
+            a_ic = a_i.copy()
+            aist = list(map(str, a_ic))
+            list_str = " ".join(aist) + "\n"
+            fin.write(list_str)
+
+            a_i.sort()
+            tmp = 0
+            for i, a in enumerate(a_i):
+                if tmp + a > B:
+                    Bs = i + 1
+                    break
+                tmp += a
+            fout.write(f"Case #{i}: {Bs}\n")
