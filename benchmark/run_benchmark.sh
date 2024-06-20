4eb393534e7cc6a56b883d1cb77aa32f05d1ab13
Andres Ladino <andres.ladino@ratp.fr>
2024-06-20 08:43:44 +0200

    fix benchmark

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,9 +1,9 @@
 poetry run python $(pwd)/benchmark/run_sol.py
 poetry run python $(pwd)/benchmark/run.py
 poetry run python $(pwd)/benchmark/benchmark.py
-sleep 10
+sleep 5
 poetry run python $(pwd)/benchmark/generate_copy.py --generate --cases 5 5 5
 poetry run python $(pwd)/benchmark/run_sol.py
 poetry run python $(pwd)/benchmark/run.py 
-poetry run python $(pwd)/benchmark/benchmark.py
-sleep 10
+poetry run python $(pwd)/benchmark/benchmark.py --P1 5 --P2 5 --P3 5
+sleep 5
